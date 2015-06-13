450774145b407f82a6399d6bbdb88c2c17688da2
Nick Johnstone <ncwjohnstone@gmail.com>
2015-06-13 17:26:30 +1200

    You can now create a project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,4 +3,16 @@ class ProjectsController < ApplicationController
 
   def index
   end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    Project.create!(
+      params.require(:project).permit(:name, :description, :contact, :url),
+    )
+
+    redirect_to :action => :index
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'projects#index'
   devise_for :users
+
+  get 'new' => 'projects#new'
+  post 'create' => 'projects#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
